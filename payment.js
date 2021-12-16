e842fab7d9b925a30f4fe838268e55cf911751a4
hley15 <93672433+hley15@users.noreply.github.com>
2021-12-15 23:10:16 -0500

    Add files via upload

---
+++ payment.js
@@ -0,0 +1,7 @@
+function myFunction1() {
+    document.getElementById("play").innerHTML = "After when you fill out your information, please<br/> come to our restaurant to pay it in cash, thank you."
+}
+
+function myFunction2() {
+    document.getElementById("demo").innerHTML = "Your food will be ready in 5-10 minutes. Please come to our restaurant for your order.<br/> (Please make sure you filled out your correct card info before you get your order)";
+}
